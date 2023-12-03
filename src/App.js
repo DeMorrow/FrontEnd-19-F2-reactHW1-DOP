73722e6e93e962417bed01796b4a054b16f77cd0
DeMorrow <amantur.dooron@gmail.com>
2023-12-03 16:18:56 +0600

    react1

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import Vergil from "./Vergil";
+import Home from "./home";
+
+function App() {
+  const [num, setNum] = useState(0);
+  return (
+    <div>
+      <h1>{num}{num == 15 ? " MAX": num == 0 ? ' MIN': '' }</h1>
+      <button onClick={() =>{
+        setNum(num + 5)
+        if (num > 14) {
+          setNum(15)
+        }
+      }}>+5</button>
+      <button onClick={() =>{
+        setNum(num - 5)
+        if (num < 1) {
+          setNum(0)
+        }
+      }}>-5</button>
+      <button onClick={() =>{
+        setNum(num + 1)
+        if(num > 14){
+          setNum(15)
+        }
+
+      }}>+</button>
+      <button onClick={() =>{
+        setNum(num - 1)
+        if(num < 1){
+          setNum(0)
+        }
+
+      }}>-</button>
+      <button onClick={() =>{
+        setNum(0)
+
+      }}>0</button>
+      <button onClick={() =>{
+        setNum(1000000000000)
+      }}>DON'T CLICK</button>
+      {/* <input id="input" type="text" placeholder="Введите число" />
+      <button onClick={() =>{
+        num.value = document.getElementById("input").value
+      }}>Set Number</button> */}
+
+{/*       
+      <Home />
+      <Vergil/>
+       */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Vergil.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Vergil = () => {
+    return (
+        <div>
+            <h1>I am the storm that is approaching
+            </h1>
+        </div>
+    );
+}
+
+export default Vergil;
